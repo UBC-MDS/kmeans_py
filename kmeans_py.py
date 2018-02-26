2df2686c919dc9dfce1f5dc8a41db27ecdfa8d28
Johannes Harmse <johannesharmse91@gmail.com>
2018-02-25 23:00:04 -0800

    Add function documentation

--- kmeans_py.py
+++ kmeans_py.py
@@ -1,35 +1,33 @@
 """User-friendly k-means clustering package"""
 
 def kmeans_init(data, K, algorithm = 'k-means++'):
-""" Choose Initial K-Means Values
+    """ Choose Initial K-Means Values
 
-Arguments
----------
-data: array
-    the data object that k-means clustering will be applied to.
+    Arguments
+    ---------
+    data: array
+        the data object that k-means clustering will be applied to.
 
-K: float
-    the number of initial values to be chosen. Should correspond to the number of clusters to be chosen.
+    K: float
+        the number of initial values to be chosen. Should correspond to the number of clusters to be chosen.
 
-algorithm: string (default = 'k-means++')
-    the initialisation algorithm specified as a string.
+    algorithm: string (default = 'k-means++')
+        the initialisation algorithm specified as a string.
 
-    - 'k-means++': K-means++ optimization algorithm. Safer, but more time complex, initialization algorithm compared to Lloyd's algorithm.
+        - 'k-means++': K-means++ optimization algorithm. Safer, but more time complex, initialization algorithm compared to Lloyd's algorithm.
 
-Returns
--------
-Array with coordinates for initialization values, where each row is an initialization value and the columns correspond with the columns of the input data object.
+    Returns
+    -------
+    Array with coordinates for initialization values, where each row is an initialization value and the columns correspond with the columns of the input data object.
 
-Examples
---------
-import numpy as np
-data = np.random.uniform(0,10,100)
-np.random.normal(5,1,100)
-data = data.frame(x = runif(100, min = 0, max = 10) + rep(c(0, 10), 50), y = rnorm(100, 5, 1) + rep(c(0, 10), 50))
+    Example
+    --------
+    import numpy as np
 
-"""
-# ` @examples
-# ` # create input data object with two distinct clusters
-# ` data = data.frame(x = runif(100, min = 0, max = 10) + rep(c(0, 10), 50), y = rnorm(100, 5, 1) + rep(c(0, 10), 50))
-# `
-# ` kmeans_init(data = data, K = 2)
+    x = np.random.uniform(0,10,100) + [0, 10]*50
+    y = np.random.normal(5,1,100) + [0, 10]*50
+    data = np.array([x, y])
+
+    kmeans_init(data, K = 2)
+
+    """
