7d3393dd042a113a17780e1b3f87b5f3b967fcbb
CHARLEY CARRIERO <carriero.char@gmail.com>
2018-03-03 16:49:41 -0800

    added plot unit testing

--- kmeans_py/test/test_kmeans_py.py
+++ kmeans_py/test/test_kmeans_py.py
@@ -5,6 +5,7 @@
 import sys
 sys.path.insert(0, '.')
 from kmeans_py import kmeans_py
+from pathlib import Path
 
 import numpy as np
 
@@ -95,3 +96,32 @@ def test_kmeans_cluster():
         assert True
     else:
         assert False
+
+
+
+def test_kmeans_plot(test_class):
+    '''
+    testing the kmeans_plot function
+
+    input: any initialized kmeans class
+    tests
+        - that input data is present and the correct type
+        - that cluster assignments have been made
+        - that each data point has been assigned a cluster
+        - an image has been saved to file
+    '''
+
+    # check to see that data has been initialized
+    # check that data is a numpy nd array
+    assert test_class.data != None
+    assert type(test_class.data) is np.ndarray
+
+    # check to see if cluster assignments have been initialized
+    assert test_class.cluster_assignments != None
+
+    # check to see that there is a cluster assigned to each point
+    assert len(test_class.cluster_assignments) == test_class.data.shape[0]
+
+    # check to see that an image has been produced
+    my_file = Path("/kmeans_plot.png")
+    assert my_file.is_file() == True
